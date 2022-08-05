2ef4286bc5732a034d44bd8cc0d2e3a6e37fc7d0
andycall <dongtiangche@outlook.com>
2022-08-06 06:45:16 +0800

    feat: fix video_player and remove camera

--- packages/kraken_camera/example/android/app/src/main/java/com/example/kraken_camera_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.kraken_camera_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- packages/kraken_camera/lib/kraken_camera.dart
+++
@@ -1,8 +0,0 @@
-import 'package:kraken/dom.dart';
-import 'camera_element.dart';
-
-class KrakenCamera {
-  static void initialize() {
-    defineElement('CAMERA-PREVIEW', (context) => CameraPreviewElement(context));
-  }
-}
